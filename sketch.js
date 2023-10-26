a6dde835485ca86ca9c60d5491a80b7a09c1e3c1
edhison paguay <edhison.paguay@ucuenca.edu.ec>
2023-10-26 16:46:59 -0500

    se agrego sonido al movimiento de las pelotas

--- sketch.js
+++ sketch.js
@@ -1,7 +1,10 @@
 let noiseTime = 0;
 let noiseTam = 100;
+let osc, playing, freq, amp;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  osc = new p5.Oscillator("triangle");
 }
 
 function draw() {
@@ -9,6 +12,12 @@ function draw() {
   let posY = map(noise(noiseTime + 10), 0, 1, 0, windowHeight);
   noiseTime += 0.01;
   noiseTam += 0.01;
+
+  osc.start();
+  freq = constrain(map(posX, 0, windowWidth, 500, 300), 300, 500);
+  amp = constrain(map(posX, height, 0, 0, 1), 0, 1);
+  osc.freq(freq, 0.01);
+  osc.amp(amp, 0.15);
 
   let tam = map(noise(noiseTam), 0, 1, 50, 5);
 

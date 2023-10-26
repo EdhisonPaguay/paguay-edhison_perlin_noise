ab5d43dc1ce6076e043c328196c95548244c4d4c
edhison paguay <edhison.paguay@ucuenca.edu.ec>
2023-10-26 12:23:38 -0500

    se cambio de color  a los circulos ademas se agrego la opcion de cambar el fono con un click

--- sketch.js
+++ sketch.js
@@ -7,18 +7,25 @@ function setup() {
 function draw() {
   let posX = map(noise(noiseTime), 0, 1, 0, windowWidth);
   let posY = map(noise(noiseTime + 10), 0, 1, 0, windowHeight);
-  noiseTime += 0.009;
+  noiseTime += 0.01;
   noiseTam += 0.01;
 
   let tam = map(noise(noiseTam), 0, 1, 50, 5);
 
-  fill(random(100, 255), random(50, 255), random(200, 220));
+  fill(182, 81, 237);
   stroke(255, 0);
   strokeWeight(4);
-  circle(posX, posY, tam);
+  circle(posX, posY, tam - 10);
 
   // Reflejar
   let posXRight = windowWidth - posX;
-  fill(random(50, 100));
-  circle(posXRight, posY, tam + 05);
+  fill(81, 255, 237);
+  circle(posXRight, posY, tam - 10);
+}
+function mousePressed() {
+  // al hacer click podemos cambiar el fondo
+  let rojo = random(255);
+  let verde = random(255);
+  let azul = random(255);
+  background(rojo, verde, azul);
 }
